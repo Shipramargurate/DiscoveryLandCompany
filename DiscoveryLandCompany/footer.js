a6772ddffc487e0e320e3dd16d411f48f89c9197
shipramargurate <shivanikumari22322@gmail.com>
2023-01-20 21:07:42 +0530

    [SHIVANI]: added WEB PAGE FOR DISCOVERY LAND

---
+++ DiscoveryLandCompany/footer.js
@@ -0,0 +1,31 @@
+const footer= document.querySelector('.footer');
+footer.innerHTML = `
+<div class="footer">
+    <div class="footer1">
+        <img class="imageiccon" src="/ASSETS/footerlogo.PNG">
+        <div class="bottemfooter">
+            <p>@ Discovery Land Company.All rights reserved</p>
+        </div>
+    </div>
+    <div class="footer2">
+        <div class="subchild1">
+            <a>GET IN TOUCH</a>
+            <a>(480)624-5200 </a>
+            <a>mail@discoveryland.com</a>
+        </div>
+        <div class="subchild2">
+            <a>About DLC</a>
+            <a>Experiences</a>
+            <a>Gallery</a>
+            <a>Press</a>
+        </div>
+        <div class="subchild2">
+            <a>magazine</a>
+            <a>careers</a>
+            <a>Foundation</a>
+            <a>Comapny</a>
+            <a>Contact</a>
+            <a>Privacy</a>
+        </div>
+    </div>
+    </div>`;
---
+++ DiscoveryLandCompany/header.js
@@ -0,0 +1,17 @@
+const header= document.querySelector('.allhead');
+header.innerHTML=` <div class="main">
+<div class="logocontent">
+    <div><img src="/ASSETS/logo.svg" title="logo" alt="logo"></div>
+</div>
+
+<div class="mainone">
+    <a href="/HTML/AboutDLC.html">About DLC</a>
+    <a href="/HTML/OurWorlds.html">Our Worlds</a>
+    <a href="/HTML/Experiences.html">Experiences </a>
+    <a href="/HTML/Gallery.html">Gallery </a>
+    <a href="#">Press </a>
+</div>
+</div>
+<div class="headertitile">
+<p>Creating your<br> perfect world</p>
+</div>`;
